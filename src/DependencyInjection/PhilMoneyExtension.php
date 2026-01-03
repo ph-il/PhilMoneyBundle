5816a42d69b7db09ce95af4fe24f6b266bb27148
Philippe Gamache <info@ph-il.ca>
2026-01-03 14:12:09 -0500

    Move from XML to YAML
    Also remove some Deprecations

--- src/DependencyInjection/PhilMoneyExtension.php
+++ src/DependencyInjection/PhilMoneyExtension.php
@@ -7,7 +7,7 @@ namespace Phil\MoneyBundle\DependencyInjection;
 use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader;
-use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\Extension\Extension;
 
 /**
  * This is the class that loads and manages your bundle configuration.
@@ -24,16 +24,16 @@ class PhilMoneyExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
 
-        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-        $loader->load('services.xml');
-        $loader->load('form_types.xml');
+        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.yml');
+        $loader->load('form_types.yml');
 
         if (in_array('twig', $config['templating']['engines'], true)) {
-            $loader->load('twig_extension.xml');
+            $loader->load('twig_extension.yml');
         }
 
         if (in_array('php', $config['templating']['engines'], true)) {
-            $loader->load('templating_helper.xml');
+            $loader->load('templating_helper.yml');
         }
 
         $this->remapParameters($config, $container, [
